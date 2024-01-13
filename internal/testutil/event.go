c6e5cadadb1f1d52bcff1f842dc0b3a011346abe
Reda Laanait <laanait.reda@gmail.com>
2024-01-13 19:13:22 +0100

    refactor: move testutil to internal

---
+++ internal/testutil/event.go
@@ -0,0 +1,94 @@
+package testutil
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/ln80/event-store/event"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func generateRandomText(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+const (
+	Dest2 = "dest2"
+)
+
+type Event1 struct {
+	Val string
+}
+type Event2 struct {
+	ID         string `pii:"subjectID"`
+	Val        string `pii:"data"`
+	LongText   string
+	MediumText string `aliases:"Medium"`
+}
+
+func (e *Event2) EvDests() []string {
+	return []string{Dest2}
+}
+
+var _ event.Publishable = &Event2{}
+
+func GenEvents(count int) []any {
+	events := make([]any, count)
+	for i := 0; i < count; i++ {
+		var evt any
+		if i%2 == 0 {
+			evt = Event2{
+				ID:         strconv.Itoa(i),
+				Val:        "val " + strconv.Itoa(i),
+				LongText:   generateRandomText(100),
+				MediumText: generateRandomText(50),
+			}
+		} else {
+			evt = Event1{"val " + strconv.Itoa(i)}
+		}
+
+		events[i] = evt
+	}
+	return events
+}
+
+func FormatEnv(env event.Envelope) string {
+	return fmt.Sprintf(`
+		stmID: %s
+		type: %s
+		evtID: %s
+		at: %v
+		ver: %v
+		gVer: %v
+		user: %s
+		data: %v
+	`, env.StreamID(), env.Type(), env.ID(), env.At().UnixNano(), env.Version(), env.GlobalVersion(), env.User(), env.Event())
+}
+
+func CmpEnv(env1, env2 event.Envelope) bool {
+	return env1.ID() == env2.ID() &&
+		env1.StreamID() == env2.StreamID() &&
+		env1.GlobalStreamID() == env2.GlobalStreamID() &&
+		env1.User() == env2.User() &&
+		env1.At().Equal(env2.At()) &&
+		env1.Version().Equal(env2.Version()) &&
+		reflect.DeepEqual(env1.Event(), env2.Event())
+}
+
+func RegisterEvent(namespace string) event.Register {
+	return event.NewRegister(namespace).
+		Set(&Event1{}).
+		Set(&Event2{})
+}
---
+++ internal/testutil/event_logging.go
@@ -0,0 +1,92 @@
+package testutil
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/ln80/event-store/event"
+)
+
+func TestEventLoggingStore(t *testing.T, ctx context.Context, store event.Store) {
+	t.Run("event logging basic operations", func(t *testing.T) {
+		streamID := event.NewStreamID(event.UID().String())
+		// test append events to stream
+		envs := event.Wrap(ctx, streamID, GenEvents(10))
+		if err := event.Stream(envs).Validate(func(v *event.Validation) {
+			v.SkipVersion = true
+		}); err != nil {
+			t.Fatalf("expect to gen a valid chunk, got err: %v", err)
+		}
+		if err := store.Append(ctx, streamID, envs); err != nil {
+			t.Fatalf("expect to append events, got err: %v", err)
+		}
+
+		// test append chunk twice
+		err := store.Append(ctx, streamID, envs)
+		if !errors.Is(err, event.ErrAppendEventsConflict) {
+			t.Fatalf("expect conflict error to occur, got: %v", err)
+		}
+
+		// test load appended chunk
+		renvs, err := store.Load(ctx, streamID)
+		if err != nil {
+			t.Fatalf("expect to load events got err %v", err)
+		}
+		if l := len(renvs); l != 10 {
+			t.Fatalf("invalid loaded events length, must be %d got: %d", 10, l)
+		}
+
+		// assert data integrity
+		for i, env := range envs {
+			if !CmpEnv(env, renvs[i]) {
+				t.Fatalf("event %d data altered %v %v", i, FormatEnv(env), FormatEnv(renvs[i]))
+			}
+		}
+
+		// test loading a sub stream
+		renvs, err = store.Load(ctx, streamID, time.Unix(0, 0), renvs[2].At())
+		if err != nil {
+			t.Fatalf("expect to load events got err %v", err)
+		}
+		if l := len(renvs); l != 3 {
+			t.Fatalf("invalid loaded events length, must be %d got: %d", 3, l)
+		}
+
+		// assert data integrity
+		for i, env := range renvs {
+			if !CmpEnv(env, envs[i]) {
+				t.Fatalf("event %d data altered %v %v", i, FormatEnv(env), FormatEnv(envs[i]))
+			}
+		}
+	})
+
+	t.Run("event logging with expired events", func(t *testing.T) {
+		streamID := event.NewStreamID(event.UID().String())
+
+		// test append events to stream
+		envs := event.Wrap(ctx, streamID, GenEvents(10), func(env event.RWEnvelope) {
+			env.SetTTL(5 * time.Microsecond)
+		})
+		if err := event.Stream(envs).Validate(func(v *event.Validation) {
+			v.SkipVersion = true
+		}); err != nil {
+			t.Fatalf("expect to generate a valid chunk, got err: %v", err)
+		}
+		if err := store.Append(ctx, streamID, envs); err != nil {
+			t.Fatalf("expect to append events, got err: %v", err)
+		}
+
+		// wait for events to expire
+		time.Sleep(10 * time.Microsecond)
+
+		renvs, err := store.Load(ctx, streamID)
+		if err != nil {
+			t.Fatalf("expect to load events got err %v", err)
+		}
+		if l := len(renvs); l != 0 {
+			t.Fatalf("invalid loaded events length, must be %d got: %d", 0, l)
+		}
+	})
+}
